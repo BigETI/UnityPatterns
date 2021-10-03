c48463f945356b567215492f3ca787099eba3481
BigETI <bigeti.ethem@gmail.com>
2021-10-03 12:15:28 +0200

    Added abstract class and interface for object scripts.

---
+++ Runtime/Abstract/AObjectScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Unity Patterns objects namespace
+/// </summary>
+namespace UnityPatterns.Objects
+{
+    /// <summary>
+    /// An abstract class that describes an object script
+    /// </summary>
+    public class AObjectScript : ScriptableObject, IObject
+    {
+        // ...
+    }
+}
---
+++ Runtime/Interfaces/IObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Unity Patterns namespace
+/// </summary>
+namespace UnityPatterns
+{
+    /// <summary>
+    /// An interface that represents an object
+    /// </summary>
+    public interface IObject : IScriptableObject
+    {
+        // ...
+    }
+}
